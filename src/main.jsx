367e0f54617199f02ee5ae8e65e38ce530332425
trnkrc <turankirac@gmail.com>
2025-07-07 16:17:21 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MetrajApp from "./MetrajApp";
+import MetrajApp from "./metrajapp"; // ✅ doğru dosya
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
